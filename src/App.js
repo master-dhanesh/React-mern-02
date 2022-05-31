bf53121b3b75a323a7d4052c1dc3249107332036
sheryianscodingschool <sheryians.coding.school@gmail.com>
2022-05-31 16:25:00 +0530

    Class Component Implementation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import Time from "./Components/Time";
+
+// const App = () => {
+//     const [toggle, setToggle] = useState(false);
+
+//     const toggletime = () => {
+//         setToggle(!toggle);
+//     };
+//     return (
+//         <div className="container mt-5 p-5 bg-light">
+//             <button onClick={toggletime} className="btn btn-dark">
+//                 {toggle ? "Hide Time" : "Show Time"}
+//             </button>
+//             <hr />
+
+//             {toggle ? <Time /> : <h3>Click to watch the live time</h3>}
+//         </div>
+//     );
+// };
+
+// export default App;
+
+import React, { Component } from "react";
 import Time from "./Components/Time";
 
-const App = () => {
-    const [toggle, setToggle] = useState(false);
-
-    const toggletime = () => {
-        setToggle(!toggle);
+class App extends Component {
+    state = {
+        toggle: false,
+        auth: false,
     };
-    return (
-        <div className="container mt-5 p-5 bg-light">
-            <button onClick={toggletime} className="btn btn-dark">
-                {toggle ? "Hide Time" : "Show Time"}
-            </button>
-            <hr />
-
-            {toggle ? <Time /> : <h3>Click to watch the live time</h3>}
-        </div>
-    );
-};
-
+    toggletime = () => {
+        this.setState({ ...this.state, toggle: !this.state.toggle });
+    };
+    render() {
+        return (
+            <div className="container mt-5 p-5 bg-light">
+                <button onClick={this.toggletime} className="btn btn-dark">
+                    {this.state.toggle ? "Hide Time" : "Show Time"}{" "}
+                </button>
+                <hr />
+                {this.state.toggle ? (
+                    <Time />
+                ) : (
+                    <h3>Click to watch the live time</h3>
+                )}
+            </div>
+        );
+    }
+}
 export default App;
