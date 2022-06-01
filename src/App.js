6b4df251da25a9226306c7cab78f0266b697f3de
sheryianscodingschool <sheryians.coding.school@gmail.com>
2022-06-01 16:38:37 +0530

    HIGHER ORDER COMPONENTS

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import { useState } from "react";
-import Changedata from "./Components/Changedata";
-import MyDate from "./Components/Date";
-import MyTime from "./Components/Time";
+import React from "react";
+import Create from "./Components/Create";
+import NavWrapper from "./helpers/NavWrapper";
 
 const App = () => {
-    const [toggle, setToggle] = useState(false);
-    const [datetime, setDatetime] = useState(new Date());
-
-    const [data, setData] = useState("App.js Data");
-
-    const toggletime = () => {
-        setToggle(!toggle);
-    };
-
-    // const changeData = (newdata) => {
-    //     setData(newdata);
-    // };
-
-    setInterval(() => {
-        setDatetime(new Date());
-    }, 1000);
-
     return (
-        <div className="container mt-5 p-5 bg-light">
-            <button onClick={toggletime} className="btn btn-dark">
-                {toggle ? "Hide Time" : "Show Time"}
-            </button>
-            <hr />
-
-            {toggle ? (
-                <>
-                    <MyDate mydate={datetime} />
-                    <MyTime mytime={datetime} />
-                    <small>Live date & time</small>
-                </>
-            ) : (
-                <h3>Click to watch the live time</h3>
-            )}
-            <hr />
-            <Changedata data={data} setData={setData}>
-                Extra data of Change Data JS file
-            </Changedata>
-        </div>
+        <NavWrapper>
+            This is Casual Data
+            <Create />
+        </NavWrapper>
     );
 };
 
