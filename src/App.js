c0133207ea736478bfc85e9db9e8ffe3057a1ae2
master-dhanesh <dhanesh1296@gmail.com>
2022-06-20 15:52:47 +0530

    REDUX SYNC ASYNC TOOLKIT

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+
 import {
     decrement,
     increment,
-    incrementByAmount,
-    incrementByAmountAsync,
-} from "./store/reducers/counterSlice";
+    changeByAmount,
+    fetchPosts,
+    fetchPostsAsync,
+} from "./store/features/counter/CounterSlice";
 
 const App = () => {
     const { value } = useSelector((state) => state.counter);
     const dispatch = useDispatch();
 
-    const OnIncrement = () => {
+    const IncrementHandler = () => {
         dispatch(increment());
     };
 
-    const OnDecrement = () => {
+    const DecrementHandler = () => {
         dispatch(decrement());
     };
 
-    const OnIncByValue = () => {
-        dispatch(incrementByAmountAsync(12));
+    const ChangeByAmountHandler = () => {
+        dispatch(changeByAmount(10));
     };
 
+    useEffect(() => {
+        dispatch(fetchPostsAsync());
+    }, []);
+
     return (
-        <div className="mt-5 container ">
-            <button onClick={OnDecrement}>-</button>
-            <span className="ms-3 me-3">{value}</span>
-            <button onClick={OnIncrement}>+</button>
-            <br />
-            <button onClick={OnIncByValue}>Increment By Value</button>
+        <div className="container mt-5 alert alert-light">
+            <button onClick={IncrementHandler} className="btn btn-primary me-3">
+                +
+            </button>
+            <span className="h1">{value}</span>
+            <button onClick={DecrementHandler} className="btn btn-danger ms-3">
+                -
+            </button>
+            <hr />
+            <button onClick={ChangeByAmountHandler} className="btn btn-dark">
+                Change By Amount
+            </button>
         </div>
     );
 };
