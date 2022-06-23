dc64b11a50533f95e77720a9b1bc670088a44a96
master-dhanesh <dhanesh1296@gmail.com>
2022-06-23 15:50:45 +0530

    FIREBASE STORAGE

--- src/App.js
+++ src/App.js
@@ -1,55 +1,59 @@
-import React from "react";
 import { app } from "./config";
 import {
-    getFirestore,
-    collection,
-    addDoc,
-    getDocs,
-    deleteDoc,
-    getDoc,
-    doc,
-} from "firebase/firestore";
-import { async } from "@firebase/util";
+    deleteObject,
+    getDownloadURL,
+    getStorage,
+    listAll,
+    ref,
+    uploadBytes,
+} from "firebase/storage";
+import { random } from "nanoid";
+
 const App = () => {
-    const db = getFirestore(app);
-    const currentCollection = collection(db, "users");
+    const storage = getStorage(app);
 
-    const addUser = async () => {
-        const docRef = await addDoc(currentCollection, {
-            name: "john doe",
-            username: "john.doe",
-        });
-        console.log(docRef);
+    const Uploadhandler = async (e) => {
+        let imagename =
+            Math.floor(Math.random() * 1000) +
+            "." +
+            e.target.files[0].name.split(".").at(-1);
+
+        const storageRef = await ref(storage, imagename);
+        await uploadBytes(storageRef, e.target.files[0]);
+        console.log("Image Uploaded");
     };
 
-    const readUsers = async () => {
-        const qs = await getDocs(currentCollection);
-        const userData = [];
-        qs.forEach((doc) => {
-            userData.push({ id: doc.id, ...doc.data() });
-            // console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    const ReadImages = async () => {
+        const listRef = await ref(storage);
+        const { items } = await listAll(listRef);
+        items.forEach(async (itemRef) => {
+            const url = await getDownloadURL(itemRef);
+            console.log(url);
         });
-        console.log(userData);
     };
 
-    const deleteUser = async () => {
-        const docRef = await doc(currentCollection, "kIxLDjUuKkHzX4rn9hfT");
-        await deleteDoc(docRef);
-        console.log("User Deleted");
+    const DeleteImage = async () => {
+        const imageRef = await ref(storage, "165.jpg");
+        // const url = await getDownloadURL(imageRef);
+        // console.log(url);
+        await deleteObject(imageRef);
+        console.log("Image Deleted");
     };
 
     return (
         <div className="container mt-5 ">
-            <button onClick={addUser} className="btn btn-primary me-3">
-                Add Firestore Doc
+            <input
+                onChange={Uploadhandler}
+                type="file"
+                className="form-control w-50 mb-3"
+            />
+
+            <button onClick={ReadImages} className="btn btn-primary me-3">
+                Read Images
             </button>
 
-            <button onClick={readUsers} className="btn btn-primary me-3">
-                Read Firestore Docs
-            </button>
-
-            <button onClick={deleteUser} className="btn btn-danger me-3">
-                Delete Firestore Docs
+            <button onClick={DeleteImage} className="btn btn-danger me-3">
+                Delete Image
             </button>
         </div>
     );
